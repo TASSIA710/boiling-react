eb5edce1449e8853a3b1e8bc2cdc55f9278b1661
Tassilo <tassia710@gmail.com>
2021-04-04 00:27:57 +0200

    Create Link.tsx

---
+++ src/Components/Link.tsx
@@ -0,0 +1,38 @@
+// Website - (c) 2021 Tassilo <tassia710@gmail.com>
+// Licensed under the MIT License.
+
+import React, {Component, CSSProperties} from "react";
+import {App} from "../index";
+
+interface LinkProperties {
+	href: string;
+	reload: boolean;
+	className: string;
+	style: CSSProperties;
+	newTab: boolean;
+}
+
+export class Link extends Component<LinkProperties> {
+
+	public static defaultProps = {
+		"reload": false,
+		"className": "",
+		style: {},
+		newTab: false,
+	};
+
+	private static clickListener(props: LinkProperties, event: React.MouseEvent) {
+		event.preventDefault();
+		App.Redirect(props.href, props.reload, props.newTab).then().catch(console.error);
+	}
+
+	public render() {
+		return <a href={this.props.href}
+				  className={this.props.className}
+				  style={this.props.style}
+				  onClick={event => Link.clickListener(this.props, event)}>
+			{this.props.children}
+		</a>;
+	}
+
+}
