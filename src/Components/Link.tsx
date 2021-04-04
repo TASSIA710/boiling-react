5623ddb4c4f04cf3ab7f7c4b40ec8eb8cc7a2504
Tassilo <tassia710@gmail.com>
2021-04-04 19:13:12 +0200

    Update Link.tsx

--- src/Components/Link.tsx
+++ src/Components/Link.tsx
@@ -7,18 +7,22 @@ import {App} from "../index";
 interface LinkProperties {
 	href: string;
 	reload: boolean;
+
 	className: string;
 	style: CSSProperties;
 	newTab: boolean;
+	title: string;
 }
 
 export class Link extends Component<LinkProperties> {
 
 	public static defaultProps = {
 		"reload": false,
+
 		"className": "",
 		style: {},
 		newTab: false,
+		title: "",
 	};
 
 	private static clickListener(props: LinkProperties, event: React.MouseEvent) {
@@ -29,7 +33,7 @@ export class Link extends Component<LinkProperties> {
 	public render() {
 		return <a href={this.props.href}
 				  className={this.props.className}
-				  style={this.props.style}
+				  style={this.props.style} title={this.props.title}
 				  onClick={event => Link.clickListener(this.props, event)}>
 			{this.props.children}
 		</a>;
